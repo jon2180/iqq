551eaff5a2ece13bbe209e2361347c62b3d30b2b
lisheng <lisheng_lover@qq.com>
2019-12-14 23:58:39 +0800

    191214第3次提交

--- src/main/java/com/nxt/im/client/ClientRouter.java
+++ src/main/java/com/nxt/im/client/ClientRouter.java
@@ -67,13 +67,5 @@ public class ClientRouter {
   }
 
 
-  /**
-   * TODO 关于线程间通信的想法
-   * 
-   * 创建另一个线程用来监测通信的过程，在里面创建一张通信记录表，类似于{线程id + id : value}
-   * 调用userReg、userLog方法的时候到一个表里去注册一个id
-   * 然后在ResponseHandler拿到返回的信息，（打印到控制台的那个地方），存到这个id的value里
-   * 然后监测进程死循环取出信息来分发给相应的线程？
-   */
 
 }
